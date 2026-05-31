d315d656ade869845954336b1c2e19cff88ab207
Sunpreet Kaur <sunpreet@Sunpreets-MacBook-Pro.local>
2026-05-31 17:58:13 +1000

    add ecommerce project code

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from 'axios';
+import { Routes, Route } from 'react-router';
+import { useState, useEffect } from 'react';
+import { HomePage } from './pages/home/HomePage';
+import { CheckoutPage } from './pages/checkout/CheckoutPage';
+import { OrdersPage } from './pages/orders/OrdersPage';
 import './App.css'
 
+// This makes axios available in the Console.
+// - Then, you can try running axios.post('/api/reset') in the Console.
+window.axios = axios;
+
+// More details:
+// - Normally, we can't access values (like axios) outside of a file.
+// - However, JavaScript has a built-in, global object called window
+//   (this represents the browser window).
+// - So one way to make a value accessible anywhere (including in the
+//   Console), is to attach it to the window object. That's why we
+//   do window.axios = axios;
+// - Now, in the Console, we can run window.axios.post(...)
+// - And JavaScript has another shortcut we can use. If we just type
+//   "axios", this is a shortcut for "window.axios"
+// - That's why the code window.axios = axios; lets us use "axios"
+//   anywhere (including in the Conosle).
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [cart, setCart] = useState([]);
+
+  const loadCart = async () => {
+    const response = await axios.get('/api/cart-items?expand=product');
+    setCart(response.data);
+  };
+
+  useEffect(() => {
+    loadCart();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} loadCart={loadCart} />} />
+      <Route path="orders" element={<OrdersPage cart={cart} loadCart={loadCart} />} />
+    </Routes>
   )
 }
 
---
+++ src/pages/checkout/OrderSummary.jsx
@@ -0,0 +1,32 @@
+import dayjs from 'dayjs';
+import { DeliveryOptions } from './DeliveryOptions';
+import { CartItemDetails } from './CartItemDetails';
+
+export function OrderSummary({ cart, deliveryOptions, loadCart }) {
+  return (
+    <div className="order-summary">
+      {deliveryOptions.length > 0 && cart.map((cartItem) => {
+        const selectedDeliveryOption = deliveryOptions
+          .find((deliveryOption) => {
+            return deliveryOption.id === cartItem.deliveryOptionId;
+          });
+
+          
+        return (
+          <div key={cartItem.productId} className="cart-item-container">
+            <div className="delivery-date">
+              Delivery date: {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
+            </div>
+
+            <div className="cart-item-details-grid">
+             
+              <CartItemDetails cartItem={cartItem} loadCart={loadCart} />
+
+              <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions} loadCart={loadCart} />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
